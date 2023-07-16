88ce450ed0b1b7b2b7bdcc1f6677fd93a7a5c62b
Rahmat-Jaas <mdrahmatullah9534@gmail.com>
2023-07-16 11:41:27 +0530

    Threads app

--- instagram-threads-main/CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-# 0.0.1
-Fully functionnal firebase Auth Development with Name, Link, Signup Screen.
-# 0.0.2
-Add Basic UI threads
-# 0.0.3
-Finish UI Home Page & Profile Page
--- instagram-threads-main/client/lib/common/locator.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get_it/get_it.dart';
-import 'package:threads/helper/shared_prefrence_helper.dart';
-
-final getIt = GetIt.instance;
-
-void setupDependencies() {
-  getIt.registerSingleton<SharedPreferenceHelper>(SharedPreferenceHelper());
-}
